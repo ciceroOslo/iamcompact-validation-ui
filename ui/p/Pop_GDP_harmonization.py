b39f3c087f7f56c419f7641fd407c1c52eed972f
Jan Ivar Korsbakken <janko@cicero.oslo.no>
2024-09-21 11:54:49 +0200

    Copying AR6 vetting file to population/GDP vetting file.
    Starting to write a new version of the population/GDP harmonization vetting
    page, using the AR6 vetting page as a basis. This version is being commited to
    be able to track which changes needed to be made to turn the page into a
    vetting page for the population/GDP harmonization, which might then serve as a
    basis for making a template or a class that can more easily be configured for
    different vetting checks.

--- ui/p/Pop_GDP_harmonization.py
+++ ui/p/Pop_GDP_harmonization.py
@@ -1,15 +1,19 @@
 from collections.abc import Mapping
+from pathlib import Path
 
 import pandas as pd
+from pandas.io.formats.style import Styler as PandasStyler
 import pyam
 import streamlit as st
 
-from iamcompact_vetting.iamcompact_outputs import \
-    gdp_pop_harmonization_output
+from iamcompact_vetting.output.base import MultiCriterionTargetRangeOutput
+from iamcompact_vetting.output.iamcompact_outputs import \
+    ar6_vetting_target_range_output
 
 from common_elements import (
     check_data_is_uploaded,
     common_instructions,
+    download_excel_output_button,
     make_passed_status_message,
 )
 from common_keys import (
@@ -19,70 +23,128 @@ from common_keys import (
 )
 
 
+# The functions below depend on a common iamcompact_vetting
+# MultiCriterionTargetRangeOutput object to compute vetting checks and to
+# produce output. It is set in the line below as a global variable (within this
+# file). Change it here if needed, rather than inside the functions.
+outputter: MultiCriterionTargetRangeOutput = ar6_vetting_target_range_output
+
 
 def main():
-
-    st.header('GDP and population harmonization assessment')
+    
+    st.header('Vetting checks for IPCC AR6')
 
     check_data_is_uploaded(stop=True, display_message=True)
     uploaded_iamdf: pyam.IamDataFrame = st.session_state[SSKey.IAM_DF_UPLOADED]
 
     status_area = st.empty()
 
-    if st.session_state.get(SSKey.GDP_POP_OUTPUT_DFS, None) is None:
-        status_area.info('Computing GDP and population harmonization checks...', icon='⏳')
-        _dfs: Mapping[str, pd.DataFrame] = \
-            compute_gdp_pop_harmonization_check(uploaded_iamdf)
-        st.session_state[SSKey.GDP_POP_ALL_PASSED] = \
-            _dfs[CriterionOutputKey.IN_RANGE].all(axis=None, skipna=True)
-        st.session_state[SSKey.GDP_POP_ALL_INCLUDED] = \
-            _dfs[CriterionOutputKey.IN_RANGE].all(axis=None, skipna=False)
-        st.session_state[SSKey.GDP_POP_OUTPUT_DFS] = _dfs
+    if st.session_state.get(SSKey.AR6_CRITERIA_OUTPUT_DFS, None) is None:
+        status_area.info('Computing IPCC AR6 vetting checks...', icon='⏳')
+        _styled_dfs: Mapping[str, PandasStyler] = \
+            compute_ar6_vetting_checks(uploaded_iamdf)
+        _dfs: Mapping[str, pd.DataFrame] = {
+            _key: _styled_df.data for _key, _styled_df in _styled_dfs.items()
+        }
+        st.session_state[SSKey.AR6_CRITERIA_ALL_PASSED] = \
+            _dfs[CriterionOutputKey.INRANGE].all(axis=None, skipna=True)
+        st.session_state[SSKey.AR6_CRITERIA_ALL_INCLUDED] = \
+            _dfs[CriterionOutputKey.INRANGE].notna().all(axis=None)
+        st.session_state[SSKey.AR6_CRITERIA_OUTPUT_DFS] = _styled_dfs
         status_area.empty()
         del _dfs
 
-    gdp_pop_harmonization_output_dfs: Mapping[str, pd.DataFrame] = \
-        st.session_state[SSKey.GDP_POP_OUTPUT]
+    ar6_vetting_output_dfs: Mapping[str, PandasStyler] = \
+        st.session_state[SSKey.AR6_CRITERIA_OUTPUT_DFS]
 
     status_area.markdown(
-        make_passed_status_message(
-            all_passed=st.session_state[SSKey.GDP_POP_ALL_PASSED],
-            all_included=st.session_state[SSKey.GDP_POP_ALL_INCLUDED],
-        )
+        '\n\n'.join([
+            make_passed_status_message(
+                all_passed=st.session_state[SSKey.AR6_CRITERIA_ALL_PASSED],
+                all_included=st.session_state[SSKey.AR6_CRITERIA_ALL_INCLUDED],
+            ),
+            'Note: In AR6, only the checks on historical values were grounds '
+            'for exclusion. The checks on future values (post-2020) were only '
+            'a flag for possible issues.',
+        ]),
+        unsafe_allow_html=True,
     )
 
-
-    summary_tab, deviation_tab, descriptions_tab = st.tabs(
-        ['Statuses', 'Deviations', 'Descriptions']
+    in_range_tab, values_tab, descriptions_tab = st.tabs(
+        ['Statuses', 'Values', 'Descriptions']
     )
-    with summary_tab:
+    _tab_data: PandasStyler
+    with in_range_tab:
         st.markdown(
-            'Agreement with harmonization data within threshold, per '
-            'model, scenario and region:'
+            'Pass status per model and scenario. '
+                '<span style="color: green"><b>✅</b></span> for passed, '
+                '<span style="color: red"><b>❌</b></span> for not passed, '
+                'blank or `None` with <span style="background-color: lightgrey">grey background</span> for not assessed (required data not present):',
+            unsafe_allow_html=True,
         )
+        _tab_data = ar6_vetting_output_dfs[CriterionOutputKey.INRANGE]
+        # _tab_data = ar6_vetting_output_dfs[CriterionOutputKey.INRANGE].format(lambda x: 'missing' if pd.isna(x) else '✅' if x==True else '❌' if x==False else '')
+        # st.write(_tab_data.to_html(), unsafe_allow_html=True)
         st.dataframe(
-            gdp_pop_harmonization_output_dfs[CriterionOutputKey.IN_RANGE]
+            # _tab_data.data.map(lambda x: 'missing' if pd.isna(x) else '✅' if x==True else '❌' if x==False else 'unknown'),
+            _tab_data.format(lambda x: 'missing' if pd.isna(x) else '✅' if x==True else '❌' if x==False else '', na_rep='missing'),
+            column_config={_col: st.column_config.TextColumn()
+                           for _col in _tab_data.data.columns}
         )
-    with deviation_tab:
+    with values_tab:
         st.markdown(
-            'Relative deviation from harmonization data, normalized to 1.0 '
-            '(i.e., 0.0 means identical with harmonization data, 0.5 means '
-            '50% higher, and -0.5 means 50% lower):'
+            'Values calculated for the vetting criteria per model and '
+            'scenario. <span style="color: violet"><b>Violet</b></span> for '
+            'numbers below range, <span style="color: red"><b>red</b></span> '
+            'for numbers above range, blank or `None` with '
+            '<span style="background-color: lightgrey">grey background</span> for not assessed (required data not present):',
+            unsafe_allow_html=True,
         )
+        _tab_data = ar6_vetting_output_dfs[CriterionOutputKey.VALUE]
         st.dataframe(
-            gdp_pop_harmonization_output_dfs[CriterionOutputKey.VALUES] - 1.0
+            _tab_data.format(thousands=' '),
+            # column_config={
+            #     _col: st.column_config.TextColumn()
+            #     for _col in _tab_data.data.columns}
         )
     with descriptions_tab:
+        st.markdown('Descriptions of each vetting criterion: ')
         st.info('Still to be added...', icon='🚧')
 
-###END def mail
+    download_excel_file_name: str = '_'.join(
+        [
+            str(Path(st.session_state[SSKey.FILE_CURRENT_NAME]).stem),
+            'AR6_vetting.xlsx',
+        ]
+    )
+    download_excel_output_button(
+        output_data=st.session_state[SSKey.AR6_CRITERIA_OUTPUT_DFS],
+        outputter=outputter,
+        download_path_key=SSKey.AR6_EXCEL_DOWNLOAD_PATH,
+        download_file_name=download_excel_file_name,
+    )
+    st.markdown(
+        'Download full results as an Excel file.\n'
+        'The file includes the "Statuses" and "Values" tabs shown here, as '
+        'well as a separate tab with both status and values for each '
+        'criterion. The file uses boolean TRUE/FALSE values rather than '
+        'checkboxes.'
+    )
+
+###END def main
 
 
-def compute_gdp_pop_harmonization_check(
+
+def compute_ar6_vetting_checks(
     iamdf: pyam.IamDataFrame
-) -> Mapping[str, pd.DataFrame]:
-    return gdp_pop_harmonization_output(iamdf)
-###END def compute_gdp_pop_harmonization_check
+) -> Mapping[str, PandasStyler]:
+    """Compute vetting checks on the IAM DataFrame."""
+    return outputter.prepare_styled_output(
+        iamdf,
+        prepare_output_kwargs=dict(add_summary_output=True),
+        style_output_kwargs=dict(include_summary=True),
+    )
+###END def compute_ar6_vetting_checks
 
 
 if __name__ == PAGE_RUN_NAME:
