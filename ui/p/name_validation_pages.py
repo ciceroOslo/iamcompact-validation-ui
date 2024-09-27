b328203c6f471e8f722da9651aa348342838484c
Jan Ivar Korsbakken <janko@cicero.oslo.no>
2024-09-27 23:22:20 +0200

    Finished making name validation pages.

--- ui/p/name_validation_pages.py
+++ ui/p/name_validation_pages.py
@@ -1,10 +1,19 @@
+from collections.abc import (
+    Callable,
+    Iterable,
+    Mapping,
+)
 import typing as tp
 
 import pandas as pd
 import pyam
 import streamlit as st
 
-from nomenclature import DataStructureDefinition
+from nomenclature import (
+    DataStructureDefinition,
+    CodeList,
+)
+from nomenclature.code import Code
 
 from iamcompact_nomenclature import get_dsd
 
@@ -26,12 +35,21 @@ def make_name_validation_dim_page(
         extra_message: tp.Optional[str] = None,
         invalid_names_dict_key: tp.Optional[SSKey] = None,
         dsd: tp.Optional[DataStructureDefinition] = None,
+        invalid_names_display_func: tp.Optional[
+            Callable[[list[str]|pd.DataFrame, str], None]
+        ] = None,
+        all_valid_names_display_func: tp.Optional[
+            Callable[[CodeList, str], None]
+        ] = None,
 ) -> None:
 
     common_setup()
 
     if invalid_names_dict_key is None:
         invalid_names_dict_key = SSKey.VALIDATION_INVALID_NAMES_DICT
+
+    if dsd is None:
+        dsd = get_validation_dsd()
 
     if run_validation_page_name is None:
         run_validation_page_name = PageName.NAME_VALIDATION_SUMMARY
@@ -71,6 +89,63 @@ def make_name_validation_dim_page(
 
     if extra_message is not None:
         st.write(extra_message)
+
+    invalid_names: list[str]|pd.DataFrame \
+        = st.session_state[invalid_names_dict_key][dim_name]
+
+    invalid_names_tab, all_valid_names_tab = st.tabs(
+        [
+            'Unrecognized names',
+            'All valid names',
+        ]
+    )
+
+    with invalid_names_tab:
+        if invalid_names_display_func is not None:
+            invalid_names_display_func(invalid_names, dim_name)
+        else:
+            if len(invalid_names) == 0:
+                st.info(
+                    f'No invalid {dim_name} names found.',
+                    icon='✅',
+                )
+            else:
+                st.write(
+                    'The following <span style="color: red"><b>unrecognized'
+                    f'</b></span> {dim_name} names were found:',
+                    unsafe_allow_html=True,
+                )
+                st.table(invalid_names)
+
+    with all_valid_names_tab:
+        if all_valid_names_display_func is not None:
+            all_valid_names_display_func(getattr(dsd, dim_name), dim_name)
+        else:
+            st.write(
+                'The following is a list of all <span style="color: green">'
+                f'recognized</span> {dim_name} names:',
+                unsafe_allow_html=True,
+            )
+            st.table(
+                make_attribute_df(
+                    getattr(dsd, dim_name),
+                    attr_names=['name', 'description', 'alpha_3', 'countries', 'note'] \
+                        if dim_name == 'region' else ['name', 'description', 'note'],
+                    column_names={
+                        'name': 'Name',
+                        'description': 'Description',
+                        'alpha_3': 'ISO-3',
+                        'countries': 'Contains',
+                        'note': 'Note'
+                    } if dim_name == 'region' else {
+                        'name': 'Name',
+                        'description': 'Description',
+                        'note': 'Note'
+                    },
+                    use_filler='',
+                )
+            )
+    
 
 ###END def make_name_validation_page
 
@@ -128,3 +203,50 @@ def get_validation_dsd(
         st.session_state[SSKey.VALIDATION_DSD] = dsd
     return dsd
 ###END def get_validation_dsd
+
+
+def make_attribute_df(
+        codelist: CodeList,
+        attr_names: tp.Optional[Iterable[str]] = None,
+        column_names: tp.Optional[Mapping[str, str]] = None,
+        use_filler: bool|tp.Any = False,
+) -> pd.DataFrame:
+    """Make a DataFrame with the specified attributes of a CodeList.
+
+    Parameters
+    ----------
+    codelist : CodeList
+        The CodeList object to get the attributes from.
+    attr_names : Iterable[str]
+        The names of the attributes to include. Optional, by default
+        `['name', 'description']`.
+    column_names : Mapping[str, str]
+        A mapping from attribute name to column name. Optional, by default uses
+        the attribute names directly as column names.
+    use_filler : bool or any
+        Whether to use filler values for items that miss the given attributes
+        (as opposed to raising an `AttributeError`). If `False` (bool literal),
+        no filler will be used, and `AttributeError` will be most likely be
+        raised if any of `attr_names` is missing for any of the code items. If
+        any other value, that value will be used as for any attributes that are
+        not found.
+
+    Returns
+    -------
+    pd.DataFrame
+        A DataFrame with the specified attributes listed in columns.
+    """
+    if attr_names is None:
+        attr_names = ['name', 'description']
+    if column_names is None:
+        column_names = dict(zip(attr_names, attr_names))
+    return pd.DataFrame(
+        data=[
+            [getattr(_code, _attr_name) for _attr_name in attr_names]
+            if use_filler==False else
+            [getattr(_code, attrname, use_filler) for attrname in attr_names]
+            for _code in codelist.values()
+        ],
+        columns=list(column_names.values()),
+    )
+###END def make_attribute_df
