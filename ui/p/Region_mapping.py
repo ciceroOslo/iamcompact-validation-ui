6f9d24909d6e5d1b73d557b1121ab0b8e210de4b
Jan Ivar Korsbakken <janko@cicero.oslo.no>
2024-09-29 09:56:55 +0200

    Preliminary version of region-mapping page.
    Still need to add the following:
      * Show a table of excluded model/region combinations
      * Add button and functionality for downloading region-processed results
    Also have not tested the latest code, since the region-mapping code and code to
    check for model-native region names still need to be added to
    iamcompact-nomenclature.

--- ui/p/Region_mapping.py
+++ ui/p/Region_mapping.py
@@ -1,6 +1,9 @@
 """Page to run reigon mapping and add it to session state before vetting."""
 
+import pyam
 import streamlit as st
+
+from iamcompact_nomenclature.mapping import map_regions
 
 from common_elements import (
     check_data_is_uploaded,
@@ -40,6 +43,61 @@ def main() -> None:
 
     check_data_is_uploaded(display_message=True, stop=True)
 
+    iam_df: pyam.IamDataFrame = st.session_state[SSKey.IAM_DF_UPLOADED]
+
+    st.info(
+        'Only recognized variable names can be included in the region '
+        'mapping. Unrecognized variable names will cause the mapping function '
+        'to fail. Please make sure that you have performed the name validation '
+        'step and fixed or removed any unrecognized variables before '
+        'proceeding.',
+        icon='ℹ️',
+    )
+
+    include_excluded = st.checkbox(
+        'Include regions/models that were excluded from region mapping in '
+        'further processing.'
+    )
+
+    if st.session_state.get(SSKey.IAM_DF_REGIONMAPPED, None) is not None:
+        st.info(
+            'You have already performed the region-mapping step. You only need '
+            'to press the button below if you for some reason want to run it '
+            'again.',
+            icon='✅'
+        )
+    run_mapping_button_area = st.empty()
+    if not run_mapping_button_area.button('Perform region mapping'):
+        st.stop()
+
+    run_mapping_button_area.empty()
+    regmapped_iam_df: pyam.IamDataFrame
+    regmap_excluded_iam_df: pyam.IamDataFrame|None = None
+    with st.spinner('Performing region mapping...'):
+        if include_excluded:
+            regmapped_iam_df, regmap_excluded_iam_df = map_regions(
+                iam_df,
+                return_excluded=include_excluded,
+            )
+        else:
+            regmapped_iam_df = map_regions(iam_df, return_excluded=False)
+
+    result_iam_df: pyam.IamDataFrame
+    if include_excluded:
+        with st.spinner('Combining results with excluded items...'):
+            result_iam_df = pyam.concat(
+                [regmapped_iam_df, regmap_excluded_iam_df]
+            )
+    else:
+        result_iam_df = regmapped_iam_df
+
+    st.session_state[SSKey.IAM_DF_REGIONMAPPED] = result_iam_df
+
+    st.info(
+        'Region mapping complete. You can now proceed with the vetting steps.',
+        icon='✅',
+    )
+    
 ###END def mail
 
 
